d5ef3b72962913a9792fa2187580d3aa44d866ee
sj <e.slavyansky@jazzserve.com>
2018-07-20 15:27:38 +0300

    Build Transaction

--- wallet-operations-advanced.go
+++ wallet-operations-advanced.go
@@ -24,4 +24,73 @@ func (b *BitGo) GetTotalBalances() (total TotalBalances, err error) {
 	return
 }
 
+// Build Transaction
+
+type BuildTransactionParams struct {
+	Recipients []Recipient `json:"recipients"`
+}
+
+type Transaction struct {
+	TxHex  string `json:"txHex"`
+	TxInfo struct {
+		NP2SHInputs   int `json:"nP2SHInputs"`
+		NSegwitInputs int `json:"nSegwitInputs"`
+		NOutputs      int `json:"nOutputs"`
+		Unspents      []struct {
+			Chain        int    `json:"chain"`
+			Index        int    `json:"index"`
+			RedeemScript string `json:"redeemScript"`
+			ID           string `json:"id"`
+			Address      string `json:"address"`
+			Value        int    `json:"value"`
+		} `json:"unspents"`
+		ChangeAddresses []string `json:"changeAddresses"`
+	} `json:"txInfo"`
+	FeeInfo struct {
+		Size           int    `json:"size"`
+		Fee            int    `json:"fee"`
+		FeeRate        int    `json:"feeRate"`
+		PayGoFee       int    `json:"payGoFee"`
+		PayGoFeeString string `json:"payGoFeeString"`
+	} `json:"feeInfo"`
+}
+
+func (b *BitGo) BuildTransaction(walletId string, params BuildTransactionParams) (transaction Transaction, err error) {
+	err = b.post(
+		fmt.Sprintf("%s/wallet/%s/tx/build",
+			b.coin,
+			walletId),
+		params,
+		&transaction)
+	return
+}
+
+// Sign Transaction
+
+type Keychain struct {
+	EncryptedPrv string `json:"encryptedPrv"`
+}
+
+type SignTransactionParams struct {
+	TxPrebuild       Transaction `json:"txPrebuild"`
+	Keychain         Keychain    `json:"keychain"`
+	WalletPassphrase string      `json:"walletPassphrase"`
+}
+
+type SignedTransaction struct {
+	TxHex string `json:"txHex"`
+}
+
+func (b *BitGo) SignTransaction(walletId string, params SignTransactionParams) (signedTransaction SignedTransaction, err error) {
+	err = b.post(
+		fmt.Sprintf("%s/wallet/%s/signtx",
+			b.coin,
+			walletId),
+		params,
+		&signedTransaction)
+	return
+}
+
+// Send Transaction
+
 // TODO
--- wallet-operations-advanced_test.go
+++ wallet-operations-advanced_test.go
@@ -14,3 +14,20 @@ func TestGetTotalBalances(t *testing.T) {
 		t.Fatal(err.Error())
 	}
 }
+
+// Build Transaction
+
+func TestBuildTransaction(t *testing.T) {
+	coin, params := getTestCoin(t)
+
+	var recipients []Recipient
+	recipients = append(recipients, Recipient{
+		Address: params.Address,
+		Amount:  0.0001 * 1e8,
+	})
+
+	_, err := coin.BuildTransaction(params.WalletId, BuildTransactionParams{Recipients: recipients})
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+}
